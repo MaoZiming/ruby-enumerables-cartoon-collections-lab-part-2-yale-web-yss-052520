3c330d6d271a52e166500e4bbdbc80ededd6b486
Ziming Mao <ziming.mao@yale.edu>
2020-05-04 18:09:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,13 +8,13 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  array.map { |string| string.capitalize() + "!"}
+  planeteer_calls.map { |string| string.capitalize() + "!"}
 end
 
 def long_planeteer_calls(planeteer_calls)
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
-  array.map { |string| string.size > 4}
+  planeteer_calls.map { |string| string.size > 4}
 end
 
 def find_valid_calls(planeteer_calls)
